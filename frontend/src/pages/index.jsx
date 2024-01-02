57311ffbcc8d24e93f608564e5d82b18a1243655
Parth Sharma <parthsharma8421@gmail.com>
2024-01-02 10:15:55 +0530

    fixes

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import Header from '@/components/header'
 import Properties from '@/components/properties'
 import api from '@/api';
+import Head from 'next/head';
 
 export const getServerSideProps = async (context) => {
     try {
@@ -16,6 +17,12 @@ export const getServerSideProps = async (context) => {
 export default function Home(props) {
     return (
         <>
+            <Head>
+                <title>Airbnb | Holiday rentals, cabins, beach houses &amp; more</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet"></link>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css" rel="stylesheet"></link>
+            </Head>
             <Header />
             <Properties properties={props.properties} />
         </>
